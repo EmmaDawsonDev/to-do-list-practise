d86d95d3671db915f3e166872e535d9526aa3252
Emma Dawson <emma.l.dawson@gmail.com>
2020-12-11 16:50:56 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <input
+      type="text"
+      placeholder="✏️ To do...	"
+      v-model="currentItem"
+      @keyup.enter="addItem"
+    />
+    <ul>
+      <list-item
+        v-for="(item, index) of toDoItems"
+        :key="index"
+        :id="index"
+        :listItem="item"
+        @removeItem="removeFromList(item)"
+      ></list-item>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from "./components/ListItem.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { ListItem },
+  data() {
+    return {
+      currentItem: "",
+      toDoItems: [],
+    };
+  },
+  methods: {
+    addItem() {
+      this.toDoItems.push(this.currentItem);
+      this.currentItem = "";
+      console.log(this.toDoItems);
+    },
+    removeFromList(name) {
+      let index = this.toDoItems.indexOf(name);
+      this.toDoItems.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.wrapper {
+  max-width: 300px;
+  margin: 20px auto;
+}
+
+body {
+  font-family: "Noto Sans JP", sans-serif;
+}
+
+input {
+  width: 100%;
+  background-color: rgb(167, 211, 255);
+  padding: 1.2rem 1rem;
+  border: none;
+  font-size: 1rem;
+  font-weight: bold;
+  color: rgb(46, 46, 46);
+}
+
+input::placeholder {
+  font-size: 1rem;
+  font-weight: bold;
+  color: rgb(46, 46, 46);
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>{{ listItem }}</p>
+    <span @click="removeItem(listItem)">X</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listItem: String,
+  },
+  methods: {
+    removeItem(item) {
+      this.$emit("removeItem", item);
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: rgb(240, 240, 240);
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+}
+
+div:nth-of-type(2n) {
+  background-color: rgb(212, 212, 212);
+}
+
+p {
+  min-width: 95%;
+}
+</style>
